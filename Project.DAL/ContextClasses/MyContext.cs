ab1a15e112e4d13e6458c9f8ee182468da29dc6e
Barkın Atıcı <brknatici@hotmail.com>
2023-03-05 18:58:40 +0300

    Emloyee classı eklendi

--- Project.DAL/ContextClasses/MyContext.cs
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -24,11 +24,14 @@ namespace Project.DAL.ContextClasses
             modelBuilder.Configurations.Add(new IssueMap());
             modelBuilder.Configurations.Add(new OrderMap());
             modelBuilder.Configurations.Add(new SaloonMap());
+            modelBuilder.Configurations.Add(new EmployeeMap());
         }
         public DbSet<Order>Orders { get; set; }
         public DbSet<Customer>Customers { get; set; }
         public DbSet<Extra>Extras { get; set; }
         public DbSet<Issue>Issues { get; set; }
         public DbSet<Saloon>Saloons { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
     }
 }
